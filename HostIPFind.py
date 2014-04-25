638e1af42d29ff511d3633a76ee0178f5a480991
Xerrot <xerrot@XerrotDeb>
2014-04-25 16:03:34 +0200

    Deteccion de interfaz

--- HostIPFind.py
+++ HostIPFind.py
@@ -1,16 +1,24 @@
 #Autor Tedezed
 import os
+import commands
+
+print "Network interface: "
+print commands.getoutput("/sbin/ifconfig | egrep -o '^[a-z].......'")
+intfz = raw_input('Enter the interface to use: ')
+comand = "/sbin/ifconfig "+intfz+" | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v '*(0|255)$'"
+iphost = commands.getoutput(comand)
+print "Your IP is: ", iphost
+raw_input('Press to continue...')
+
+net = iphost.split(".")
 listt = []
 num = 1
-net = raw_input("Network Address: ")
-net = net.split(".")
-net = net[0]+"."+net[1]+"."+net[2]+"."
 while num <= 255 :
 	numstr = str(num)
-	comand = "ping -c 1 -n -i 0.2 -W 1 " + net + numstr
+	comand = "ping -c 1 -n -i 0.2 -W 1 " + net[0]+"."+net[1]+"."+net[2]+"." + numstr
 	comand = os.system(comand)
 	if comand == 0:
-		comand = net+numstr
+		comand = net[0]+"."+net[1]+"."+net[2]+"."+numstr
 		listt.append(comand)
 	num = num + 1
 print "List of addresses used in the network: "
